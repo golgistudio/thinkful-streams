dc35133c0f84bf0b3fc957195e67ddd389fcaf2b
golgistudio <lauriesreynolds@gmail.com>
2016-03-03 12:45:20 -0800

    add get related artists

--- spotify-recommend/initialServer.js
+++ spotify-recommend/initialServer.js
@@ -4,7 +4,6 @@ var events = require('events');
 
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
-    console.log('getFromApi - ' + endpoint)
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
            .end(function(response) {
@@ -21,28 +20,6 @@ var getFromApi = function(endpoint, args) {
 var app = express();
 app.use(express.static('public'));
 
-function searchRelated() {
-
-    this.search = function(artist, res) {
-        var endpoint = 'artists/' + artist.id + '/related-artists'
-        var searchID = getFromApi(endpoint)
-
-    searchID.on('end', function(item) {
-        artist.related = item.artists;
-        console.log(item.artists)
-        res.json(artist);
-       
-    });
-
-    searchID.on('error', function(code) {
-        res.sendStatus(code);
-    });
-}
-
-}
-
-var related = new searchRelated() 
-
 app.get('/search/:name', function(req, res) {
     var searchReq = getFromApi('search', {
         q: req.params.name,
@@ -52,10 +29,7 @@ app.get('/search/:name', function(req, res) {
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        related.search(artist, res)
-        
-        
-
+        res.json(artist);
     });
 
     searchReq.on('error', function(code) {
